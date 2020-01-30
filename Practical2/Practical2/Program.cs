c5ac978c08c8727e8e11b6fc98b784343399c1d9
Hardik Bhuva <hpbhuva601@gmail.com>
2020-01-30 19:48:37 +0530

    Inital Commit

---
+++ Practical2/Practical2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parctical2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i > 0; i--)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
